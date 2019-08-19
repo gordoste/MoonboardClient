d2c95a2414b5e105093d35d3f677222e85dac55d
gordoste <gordoste@iinet.net.au>
2019-08-19 22:46:12 +1000

    wip - functions for reading data files. Broken

--- src/MoonboardUtils.cpp
+++ src/MoonboardUtils.cpp
@@ -1,5 +1,128 @@
 #include "MoonboardUtils.h"
 
+//void MoonboardUtils::setBuf(char *buf, uint8_t bufLen) {
+  //m_buf = buf;
+  //m_bufLen = bufLen;
+//}
+
+void MoonboardUtils::beginCatType(char *catType) {
+  catTypeNames[numCatTypes] = catType;
+  catNameStartIdxPerType[numCatTypes] = numCatNames;
+  catCountPerType[numCatTypes] = 0;
+  selectedCatPerType[numCatTypes] = -1;
+}
+
+void MoonboardUtils::endCatType() {
+  numCatTypes++;
+}
+
+void MoonboardUtils::addCat(const char *catName) {
+  catNames[numCatNames++] = strcpy(t_catBufPtr, catName);
+  t_catBufPtr += strlen(catName)+1; // +1 for null terminator
+  catCountPerType[numCatTypes]++;
+}
+
+// Pass colon-delimited string
+// First token is cat type, others are taken as category names
+void MoonboardUtils::defineCatType(const char *catType) {
+  strcpy(m_buf, catType);
+  char t[2] = ":";
+  char *p = strtok(m_buf, t);
+  beginCatType(p);
+  while ((p = strtok(NULL, t))) {
+    addCat(p);
+  }
+  endCatType();
+}
+
+void MoonboardUtils::selectCat(uint8_t z_catType, uint8_t z_catNum) {
+  if (z_catType >= numCatTypes) { Serial.printf("Bad catType #%d\n", z_catType); }
+  if (z_catNum >= catCountPerType[z_catType]) {
+    Serial.printf("Bad catNum #%d >= %d\n", z_catNum, catCountPerType[z_catType]);
+  }
+  selectedCatPerType[z_catType] = z_catNum;
+}
+
+void MoonboardUtils::unselectCat(uint8_t z_catType) {
+  if (z_catType >= numCatTypes) { Serial.printf("Bad catType #%d\n", z_catType); }
+  selectedCatPerType[z_catType] = -1;
+}
+
+void MoonboardUtils::getListNameFromCats(char *buf, uint8_t bufLen) {
+  uint8_t copied = 0;
+  for (uint8_t l_catTypeNum = 0; l_catTypeNum < numCatTypes; l_catTypeNum++) {
+    char *l_catName;
+    if (selectedCatPerType[l_catTypeNum] == -1) {
+      l_catName = wildcardStr;
+    }
+    else {
+      l_catName = catNames[catNameStartIdxPerType[l_catTypeNum] + selectedCatPerType[l_catTypeNum]];
+    }
+    uint8_t len = strlen(l_catName);
+    if (len + 2 > bufLen) { // add delimiter and null-terminator
+      Serial.printf("gLNFC - %s too long for buffer\n", l_catName);
+      return;
+    }
+    strcpy(&(buf[copied]), l_catName);
+    copied += len;
+    if (numCatTypes - l_catTypeNum > 1) { // Skip delimiter on last one
+      strcpy(&(buf[copied++]), "_");
+    }
+  }
+}
+void MoonboardUtils::findLists() {
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+  char *listNameBufPtr = listNameBuf; // Point to unused storage for list names
+  uint8_t listNum = 0; // Keep track of how many list names we've added
+  while (file) {
+    uint8_t l = strlen(file.name());
+    if (strncmp(&(file.name()[l-4]),".dat",4) == 0) {
+      if (l-5 > MAX_LISTNAME_SIZE) { // 5 = 1 (/ prefix) + 4 (.dat suffix)
+        Serial.printf("List name %s too long\n", file.name());
+      }
+      else {
+        // Copy the list name into the space reserved and record where we copied it
+        listNames[listNum++] = strncpy(listNameBufPtr, &(file.name()[1]), l-5);
+        listNameBufPtr[l-5] = '\0'; // Null terminate the string
+        listNameBufPtr = &(listNameBufPtr[l-4]); // Move our pointer along to the next free space
+      }
+    }
+    file = root.openNextFile();
+  }
+}
+
+void MoonboardUtils::openList(const char *listName, const char *sortOrder) {
+  strcpy(m_buf, "/");
+  strcat(m_buf, listName);
+  strcat(m_buf, "_");
+  strcat(m_buf, sortOrder);
+  strcat(m_buf, ".lst");
+  File list = SPIFFS.open(m_buf);
+  strcpy(m_buf, "/");
+  strcat(m_buf, listName);
+  strcat(m_buf, ".dat");
+  File data = SPIFFS.open(m_buf);
+}
+
+void MoonboardUtils::closeList() {
+  list.close();
+  data.close();
+}
+
+void MoonboardUtils::readNextProblem(Problem *prob) {
+  const char t[2] = ":";
+  char *p;
+  int offset;
+  list.readStringUntil('\n').toCharArray(m_buf, m_bufLen);
+  p = strtok(m_buf, t);
+  p = strtok(NULL, t);
+  offset = atoi(p);
+  data.seek(offset, SeekSet);
+  data.readStringUntil('\n').toCharArray(m_buf, m_bufLen);
+  readProblem(prob, m_buf);
+}
+
 // read the string, placing data in the problem struct passed. Return true on success
 bool MoonboardUtils::readProblem(Problem *p, char *in)
 {
--- src/MoonboardUtils.h
+++ src/MoonboardUtils.h
@@ -2,10 +2,56 @@
 #define _MOONBOARD_UTILS_H
 
 #include "Data.h"
+#include <SPIFFS.h>
 
-namespace MoonboardUtils
+#define MAX_LISTS 20
+#define MAX_LISTNAME_SIZE 15
+#define CAT_BUF_SIZE 256
+#define MAX_CATS 20
+#define MAX_CAT_TYPES 5
+
+#ifndef MB_WILDCARD_STRING
+#define MB_WILDCARD_STRING "Any"
+#endif
+
+#define MB_BUFFER_SIZE 255
+
+class MoonboardUtils
 {
+public:
+  MoonboardUtils() {};
   bool readProblem(Problem *p, char *in);
+  void beginCatType(char *catType);
+  void endCatType();
+  void addCat(const char *catName);
+  void defineCatType(const char *catType);
+  void selectCat(uint8_t z_catType, uint8_t z_catNum);
+  void unselectCat(uint8_t z_catType);
+  void getListNameFromCats(char *buf, uint8_t bufLen);
+  void findLists();
+  void openList(const char *listName, const char *sortOrder);
+  void closeList();
+  void readNextProblem(Problem *p);
+protected:
+  // Internal variables
+  char *listNames[MAX_LISTS]; // Pointers to the list names
+  char wildcardStr[4] = MB_WILDCARD_STRING;
+  char catBuf[CAT_BUF_SIZE] = ""; // Storage for category names
+  char *catNames[MAX_CATS]; // Pointers to the category names
+  uint8_t numCatNames = 0;
+  uint8_t numCatTypes = 0;
+  char *catTypeNames[MAX_CAT_TYPES];
+  uint8_t catNameStartIdxPerType[MAX_CAT_TYPES]; // For each type, what index its list of names starts
+  uint8_t catCountPerType[MAX_CAT_TYPES]; // For each type, how many categories
+  int8_t selectedCatPerType[MAX_CAT_TYPES]; // For each type, which category is selected. 0-based. -1 = NONE
+private:
+  // TEMPORARY VARIABLES (USED FOR A WHILE BUT THEN WORTHLESS)
+  char *t_catBufPtr = catBuf; // Point to unused storage
+  File list;
+  File data;
+  char m_buf[MB_BUFFER_SIZE];
+  uint8_t m_bufLen = MB_BUFFER_SIZE;
+  char listNameBuf[MAX_LISTS * MAX_LISTNAME_SIZE] = ""; // Storage for the list names
 };
 
 #endif // #ifndef _MOONBOARD_UTILS_H
