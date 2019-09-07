feeca451235cb99e1e2a841fceb075bdba9ccacd
gordoste <gordoste@iinet.net.au>
2019-09-07 16:47:06 +1000

    Add menu for problem selection

---
+++ src/MoonboardMenu.cpp
@@ -0,0 +1,41 @@
+#include "MoonboardMenu.h"
+
+char MoonboardStreamMenu::problemChoice(Problem probs[], uint8_t numProblems,
+  const char *otherChoiceStrings[], const char *otherChoices) {
+  while (1) {
+    if (numProblems > 0) {
+      for (_t_ui8 = 1; _t_ui8 <= numProblems; _t_ui8++) {
+        m_stdout->printf("%d) ", _t_ui8);
+        problemAsString(&(probs[_t_ui8-1]), problemStr, sizeof(problemStr));
+      }
+    }
+    else {
+      m_stdout->println(F("No problems available for selection."));
+    }
+    uint8_t numChoices = strlen(otherChoices);
+    for (uint8_t i = 0; i < numChoices; i++) {
+      m_stdout->printf("%c) %s\n", otherChoices[i], otherChoiceStrings[i]);
+    }
+    m_stdout->printf("> ");
+    char c = m_stdin->timedRead();
+    m_stdout->println(c);
+    if (c > '0' && c <= '9' && c-'1' <= numProblems) {  // A number that's valid
+      return c;        
+    }
+    if (c == 'x' || c == 'X') return -1;
+    if (strchr(otherChoices, c)) return c; // Check if character is a valid selection
+    if (c >= 'a' && c <= 'z' && strchr(otherChoices, c - 'a' + 'A')) return c; // if LC, check for UC version
+    if (c >= 'A' && c <= 'Z' && strchr(otherChoices, c - 'A' + 'a')) return c; // if UC, check for LC version
+    m_stdout->println(F("Invalid selection."));
+  }
+  return -1;
+}
+
+#ifdef TFT_ENABLED
+
+char MoonboardTFTMenu::problemChoice(Problem probs[], uint8_t numProblems,
+  const char *otherChoiceStrings[], const char *otherChoices) {
+  return 0;
+}
+
+#endif //#ifdef TFT_ENABLED
---
+++ src/MoonboardMenu.h
@@ -0,0 +1,31 @@
+#ifndef _MOONBOARD_MENU_H
+#define _MOONBOARD_MENU_H
+
+#include "Data.h"
+#include "Menu.h"
+
+class MoonboardStreamMenu : public StreamMenu {
+  public:
+    char problemChoice(Problem probs[], uint8_t numProblems,
+      const char *otherChoiceStrings[], const char *otherChoices);
+  protected:
+    uint8_t _t_ui8;
+    char problemStr[80];
+};
+
+#ifdef TFT_ENABLED
+
+#include "TFT_eSPI.h"
+
+class MoonboardTFTMenu : public TFTMenu {
+  public:
+    char problemChoice(Problem probs[], uint8_t numProblems,
+      const char *otherChoiceStrings[], const char *otherChoices);
+  protected:
+    uint8_t _t_ui8;
+    char problemStr[80];
+};
+
+#endif //#ifdef TFT_ENABLED
+
+#endif //#ifndef _MOONBOARD_MENU_H
