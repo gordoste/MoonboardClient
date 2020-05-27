f64698907dddbd12c0f8561e9a83a62ceb398ca2
gordoste <gordoste@iinet.net.au>
2020-05-27 21:57:59 +1000

    Error checking

--- src/MBConfig.cpp
+++ src/MBConfig.cpp
@@ -1,20 +1,19 @@
 #include "MBConfig.h"
 
 MBConfigData *MBConfig::read(File &configFile) {
-    m_data.top_panel_ip.fromString(configFile.readStringUntil(':'));
-    m_data.mid_panel_ip.fromString(configFile.readStringUntil(':'));
-    m_data.btm_panel_ip.fromString(configFile.readStringUntil(':'));
+    if (!m_data.top_panel_ip.fromString(configFile.readStringUntil(':'))) return NULL;
+    if (!m_data.mid_panel_ip.fromString(configFile.readStringUntil(':'))) return NULL;
+    if (!m_data.btm_panel_ip.fromString(configFile.readStringUntil(':'))) return NULL;
     m_data.flags = atoi(configFile.readStringUntil(':').c_str());
     return &m_data;
 }
 
 bool MBConfig::write(File &configFile) {
-    configFile.printf("%s:%s:%s:%i:",
+    return configFile.printf("%s:%s:%s:%i:",
                       m_data.top_panel_ip.toString().c_str(),
                       m_data.mid_panel_ip.toString().c_str(),
                       m_data.btm_panel_ip.toString().c_str(),
-                      m_data.flags);
-    return true;
+                      m_data.flags) > 0;
 }
 
 void MBConfig::setFlags(bool testMode) {
