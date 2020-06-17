0bcf307bca6659b4e73e2cd77c6e0180194c3cc1
gordoste <gordoste@iinet.net.au>
2020-06-17 20:54:03 +1000

    Add non-blocking mode

--- src/PanelClient.cpp
+++ src/PanelClient.cpp
@@ -1,7 +1,8 @@
 #include "PanelClient.h"
 
-void PanelClient::begin(BasicLog *_log) {
+void PanelClient::begin(BasicLog *_log, uint32_t ackTimeout_ms) {
     m_log = _log;
+    m_ackTimeout = ackTimeout_ms;
 }
 
 void PanelClient::stop() {
@@ -9,7 +10,8 @@ void PanelClient::stop() {
     m_client.stop();
 }
 
-bool PanelClient::sendCommand(const char *cmd, const char *data) {
+// Returns ID of command sent, 0 on failure
+int PanelClient::sendCommand(const char *cmd, const char *data, bool blocking) {
     m_log->debug2("sendCommand #%d (%s %s)", m_cmdId, cmd, data ? data : "");
     sprintf(sendBuf, "%s %d", cmd, m_cmdId);
     m_log->debug3("write:%s", sendBuf);
@@ -20,27 +22,39 @@ bool PanelClient::sendCommand(const char *cmd, const char *data) {
         m_client.write(sendBuf);
     }
     m_client.write('\n');
-    if (!waitForAck(m_cmdId)) {
-        m_log->debug("ack #%d FAIL", m_cmdId);
-        return false;
+    if (blocking) {
+        if (!waitForAck(m_cmdId)) return 0;
+        m_log->debug2("ack #%d OK", m_cmdId);
     }
-    m_log->debug2("ack #%d OK", m_cmdId);
-    m_cmdId++;
-    return true;
+    return m_cmdId++;
 };
 
-void PanelClient::clearBoard() {
-    sendCommand("CLR");
+// Returns ID of command sent, 0 on failure
+int PanelClient::clear(bool blocking) {
+    return sendCommand("CLR", NULL, blocking);
 };
 
-void PanelClient::lightHolds(const char *holdList) {
-    sendCommand("SET", holdList);
+// Returns ID of command sent, 0 on failure
+int PanelClient::lightHolds(const char *holdList, bool blocking) {
+    return sendCommand("SET", holdList, blocking);
 };
 
+// Returns ID of command sent, 0 on failure
+int PanelClient::reset(bool blocking) {
+    return sendCommand("RST", NULL, blocking);
+}
+
+// Returns ID of command sent, 0 on failure
+int PanelClient::ping(bool blocking) {
+    return sendCommand("PING", NULL, blocking);
+}
+
+// Blocking call which waits for ACK with specified ID to be received
 bool PanelClient::waitForAck(int cmdId) {
     m_log->debug2("waitForAck(%d)", cmdId);
     while (true) {
         if (receiveLine(PANEL_RCV_TIMEOUT * 1000) == 0) {
+            m_log->debug("WFA timeout");
             return false;
         }
         m_log->debug3("wfa:%s", rcvdBuf);
@@ -61,6 +75,7 @@ bool PanelClient::waitForAck(int cmdId) {
     }
 };
 
+// Blocking call which waits for a line to be received
 int PanelClient::receiveLine(uint32_t timeout) {
     if (m_rcvLen == 0) {
         memset(rcvdBuf, 0, PANEL_RCVBUF_LEN);
@@ -88,3 +103,46 @@ int PanelClient::receiveLine(uint32_t timeout) {
     rcvdBuf[m_rcvLen] = '\0';
     return m_rcvLen;
 };
+
+// Use with non-blocking sendCommand() - call in a loop to check for ACKs
+void PanelClient::receive() {
+    if (!m_client.connected()) return;
+    if (m_rcvLen == 0) memset(rcvdBuf, 0, PANEL_RCVBUF_LEN);
+    char c;
+    while (m_client.available()) {
+        c = m_client.read();
+        switch (c) {
+        case '\r':
+            break;
+        case '\n':
+            rcvdBuf[m_rcvLen] = '\0';
+            m_rcvLen = 0;
+            if (strncmp("ACK ", rcvdBuf, 4) == 0) {
+                int id = atoi(&(rcvdBuf[4]));
+                if (id > 0) {
+                    for (auto it = m_pendingAcks.begin(); it != m_pendingAcks.end(); it++) {
+                        if ((*it).cmdId == id) m_pendingAcks.erase(it);
+                    }
+                    return;
+                }
+            }
+            m_log->error("receive() bad fmt: '%s'", rcvdBuf);
+            return;
+            break;
+        default:
+            rcvdBuf[m_rcvLen++] = c;
+        }
+    }
+}
+
+bool PanelClient::hasAckPending(int _cmdId) {
+    uint32_t now = millis();
+    // Time out ACKs - they will be in order of receipt
+    for (auto it = m_pendingAcks.begin(); it != m_pendingAcks.end() && now > ((*it).timeReceived + m_ackTimeout); it++) {
+        m_pendingAcks.erase(it);
+    }
+    for (auto it = m_pendingAcks.begin(); it != m_pendingAcks.end(); it++) {
+        if ((*it).cmdId == _cmdId) return true;
+    }
+    return false;
+}
--- src/PanelClient.h
+++ src/PanelClient.h
@@ -15,6 +15,11 @@
 #define PANEL_CONN_TIMEOUT 5 // Connect timeout (s)
 #define PANEL_RCV_TIMEOUT 10  // Receive timeout (s)
 
+typedef struct {
+    int cmdId;
+    uint32_t timeReceived;
+} ack_t;
+
 class PanelClient {
 protected:
     WiFiClient m_client;
@@ -23,20 +28,35 @@ protected:
     int m_rcvLen = 0;
     char rcvdBuf[PANEL_RCVBUF_LEN];
     char sendBuf[PANEL_SNDBUF_LEN];
+    uint32_t m_ackTimeout;
+    std::vector<ack_t> m_pendingAcks;
 
 public:
     PanelClient(){};
-    void begin(BasicLog *_log);
+    void begin(BasicLog *_log, uint32_t ackTimeout_ms = 60000);
     void stop();
-    bool sendCommand(const char *cmd, const char *data = NULL);
-    void clearBoard();
-    void lightHolds(const char *holdList);
+
+    int clear(bool blocking = true);
+    int lightHolds(const char *holdList, bool blocking = true);
+    int reset(bool blocking = true);
+    int ping(bool blocking = true);
+
+    int sendCommand(const char *cmd, const char *data = NULL, bool blocking = true);
+
+    // Blocking mode
     bool waitForAck(int cmdId);
     int receiveLine(uint32_t timeout);
+
+    // Non-blocking mode. Call receive() in a loop, checking hasAckPending()
+    void receive();
+    bool hasAckPending(int cmdId);
+
     BasicLog *getLog() { return m_log; };
     void setLog(BasicLog *l) { m_log = l; };
     WiFiClient getClient() { return m_client; };
     void setClient(WiFiClient c) { m_client = c; };
+    uint32_t getAckTimeout() { return m_ackTimeout; }
+    void setAckTimeout(uint32_t _ackTimeout_ms) { m_ackTimeout = _ackTimeout_ms; }
 };
 
 #endif // #ifndef _PANEL_CLIENT_H
