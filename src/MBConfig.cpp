c1264ccac9c938101bcff739315c43a40a57d371
gordoste <gordoste@iinet.net.au>
2020-06-26 20:19:12 +1000

    New config

--- src/MBConfig.cpp
+++ src/MBConfig.cpp
@@ -1,82 +1,45 @@
 #include "MBConfig.h"
 
 MBConfigData *MBConfig::readImpl(Stream &in, MBConfigData *dest) {
-    if (!dest->top_panel_ip.fromString(in.readStringUntil(':'))) return NULL;
-    dest->top_port = atoi(in.readStringUntil('|').c_str());
-    if (!dest->mid_panel_ip.fromString(in.readStringUntil(':'))) return NULL;
-    dest->mid_port = atoi(in.readStringUntil('|').c_str());
-    if (!dest->btm_panel_ip.fromString(in.readStringUntil(':'))) return NULL;
-    dest->btm_port = atoi(in.readStringUntil('|').c_str());
-    dest->flags = atoi(in.readStringUntil('|').c_str());
+    for (uint8_t i = 0; i < MBCONFIG_NUM_HOLDMAPS; i++) {
+        dest->holdMaps[i].src[0] = (char)in.read();
+        dest->holdMaps[i].src[1] = (char)in.read();
+        in.read();
+        dest->holdMaps[i].dest[0] = (char)in.read();
+        dest->holdMaps[i].dest[1] = (char)in.read();
+        in.read();
+    }
+    String s = in.readStringUntil('\n');
+    dest->flags = atoi(s.c_str());
     return dest;
 }
 
 bool MBConfig::writeImpl(Stream &out, MBConfigData *src) {
-    return out.printf("%s:%i|%s:%i|%s:%i|%i|",
-                      src->top_panel_ip.toString().c_str(),
-                      src->top_port,
-                      src->mid_panel_ip.toString().c_str(),
-                      src->mid_port,
-                      src->btm_panel_ip.toString().c_str(),
-                      src->btm_port,
-                      src->flags) > 0;
+    for (uint8_t i = 0; i < MBCONFIG_NUM_HOLDMAPS; i++) {
+        out.printf("%c%c:%c%c|",
+        src->holdMaps[i].src[0], src->holdMaps[i].src[1],
+        src->holdMaps[i].dest[0], src->holdMaps[i].dest[1]);
+    }
+    out.printf("%i\n", src->flags);
+    return true;
 }
 
 bool MBConfig::writeHumanReadable(Stream &out, MBConfigData *src) {
     if (src == NULL) src = &m_data;
-    return out.printf("Top: %s:%i\r\nMiddle: %s:%i\r\nBottom: %s:%i\r\nFlags: %i\r\n",
-                      src->top_panel_ip.toString().c_str(),
-                      src->top_port,
-                      src->mid_panel_ip.toString().c_str(),
-                      src->mid_port,
-                      src->btm_panel_ip.toString().c_str(),
-                      src->btm_port,
-                      src->flags) > 0;
+    out.print("Holdmaps:");
+    for (uint8_t i = 0; (i < MBCONFIG_NUM_HOLDMAPS) && (src->holdMaps[i].src[0] != ' '); i++) {
+        if (i > 0) out.print(", ");
+        out.printf("%c%c -> %c%c",
+        src->holdMaps[i].src[0], src->holdMaps[i].src[1],
+        src->holdMaps[i].dest[0], src->holdMaps[i].dest[1]);
+    }
+    out.printf("\r\nFlags: %i\r\n", src->flags);
+    return true;
 }
 
 void MBConfig::setFlags(bool testMode, MBConfigData *dest) {
     if (dest == NULL) dest = &m_data;
-    dest->flags = (testMode ? MBCONFIG_TEST_MODE : 0x0); // OR any other flags here
-}
-
-bool MBConfig::testMode(MBConfigData *dest) {
-    if (dest == NULL) dest = &m_data;
-    return dest->flags & MBCONFIG_TEST_MODE;
-}
-
-bool MBConfig::parseTopIPAndPort(const char *str, MBConfigData *dest) {
-    return parseIPAndPort(str, dest->top_panel_ip, dest->top_port);
-}
-
-bool MBConfig::parseMidIPAndPort(const char *str, MBConfigData *dest) {
-    return parseIPAndPort(str, dest->mid_panel_ip, dest->mid_port);
-}
-
-bool MBConfig::parseBtmIPAndPort(const char *str, MBConfigData *dest) {
-    return parseIPAndPort(str, dest->btm_panel_ip, dest->btm_port);
-}
-
-// Parse a string that should contain IP:port into given variables
-bool MBConfig::parseIPAndPort(const char *str, IPAddress &dstIP, uint16_t &dstPort) {
-#define IP_PORT_MAXLEN 24 // 3+1+3+1+3+1+3 + 1 + 5 + 1 (plus couple extra)
-    char tmpBuf[IP_PORT_MAXLEN];
-    IPAddress newAddr;
-    int newport = -1;
-    char *ptr;
-    uint8_t len;
-    ptr = strchr(str, '|');
-    len = (ptr == NULL ? strlen(str) : ptr - str);
-    strncpy(tmpBuf, str, len);
-    tmpBuf[len] = '\0';
-    ptr = StringUtils::strtoke(tmpBuf, ":");
-    if (ptr == NULL) return false;
-    if (!newAddr.fromString(ptr)) return false;
-    ptr = StringUtils::strtoke(NULL, "|");
-    newport = atoi(ptr);
-    if (newport == -1) return false;
-    dstIP = newAddr;
-    dstPort = newport;
-    return true;
+    dest->flags = testMode && MBCONFIG_FLAG_TEST_MODE; // OR additional flags
 }
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_MBCONFIG)
--- src/MBConfig.h
+++ src/MBConfig.h
@@ -2,36 +2,35 @@
 #define _MBCONFIG_H
 
 #include "FS.h"
-#include "IPAddress.h"
 #include "StringUtils.h"
 
 #include "ConfConf.h"
 
-#define MBCONFIG_TEST_MODE 0x1
+#define MBCONFIG_FLAG_TEST_MODE 0x1
+
+#define MBCONFIG_NUM_HOLDMAPS 5
+
+typedef struct {
+    char src[2];
+    char dest[2];
+} holdmap_t;
 
 struct MBConfigData {
-    IPAddress top_panel_ip;
-    uint16_t top_port;
-    IPAddress mid_panel_ip;
-    uint16_t mid_port;
-    IPAddress btm_panel_ip;
-    uint16_t btm_port;
-    uint8_t flags;
+    holdmap_t holdMaps[MBCONFIG_NUM_HOLDMAPS];
+    uint8_t flags = 0;
+    MBConfigData() {
+        memset(&holdMaps, ' ', sizeof(holdMaps));
+    }
 };
 
-class MBConfig : ConfConf<MBConfigData> {
+class MBConfig : public ConfConf<MBConfigData> {
 public:
     bool writeHumanReadable(Stream &out, MBConfigData *src = NULL);
     void setFlags(bool testMode, MBConfigData *dest = NULL);
-    bool testMode(MBConfigData *dest = NULL);
-    bool parseTopIPAndPort(const char *str, MBConfigData *dest = NULL);
-    bool parseMidIPAndPort(const char *str, MBConfigData *dest = NULL);
-    bool parseBtmIPAndPort(const char *str, MBConfigData *dest = NULL);
 
 private:
     MBConfigData *readImpl(Stream &in, MBConfigData *dest);
     bool writeImpl(Stream &out, MBConfigData *src);
-    bool parseIPAndPort(const char *str, IPAddress &dstIP, uint16_t &dstPort);
 };
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_MBCONFIG)
