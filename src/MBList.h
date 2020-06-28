a1e70952bf28754f2a43035fff7f9dd12c732e95
gordoste <gordoste@iinet.net.au>
2020-06-28 16:42:23 +1000

    First working version of addProblem

--- src/MBList.h
+++ src/MBList.h
@@ -5,8 +5,6 @@
 #include "StringUtils.h"
 #include <FS.h>
 
-typedef std::function<bool(Problem *prob, char *buf)> ProblemParser;
-
 enum ListType {
     LIST_CUSTOM,
     LIST_FILTER
@@ -14,7 +12,7 @@ enum ListType {
 
 class MBList {
 public:
-    void begin(char *buf, uint16_t bufLen, FS *FS, Print *stdErr);
+    void begin(char *probBuf, uint16_t probBufLen, FS *FS, Print *stdErr);
     virtual bool open(ListType type, const char *listName, const char *sortOrder);
     void close();
     bool isOpen();
@@ -28,21 +26,27 @@ public:
     uint8_t readNextPage(Problem pArr[]);
     uint8_t readPrevPage(Problem pArr[]);
     uint8_t readPage(Problem pArr[], uint16_t pageNum);
-    void setProblemParser(ProblemParser _pp) { probParser = _pp; }
+    bool addProblem(const Problem *p, const char *listName, const std::vector<SortOrder *> *sortOrders);
 
 protected:
     virtual bool seekPage(uint16_t pageNum);
     FS *m_fs;
     Print *m_stdErr;
-    char *m_buf;
-    uint16_t m_bufLen;
-    ProblemParser probParser;
+    char *m_probBuf;
+    uint16_t m_probBufLen;
+    char tmpBuf[512];
     File listFile, dataFile;
     bool listHasNext = false;
     uint16_t nextProbNum = 0;
     uint16_t listSize = 0;
     const uint8_t CONST_PAGE_SIZE = PROB_PAGE_SIZE;
     std::vector<uint32_t> pageOffsets = std::vector<uint32_t>();
+    bool openListFile(ListType type, const char *listName, const char *sortOrder);
+    bool openDataFile(ListType type, const char *listName);
+    bool listFileNameToBuf(ListType type, const char *listName, const char *sortOrder, char *buf, size_t bufLen);
+    bool dataFileNameToBuf(ListType type, const char *listName, char *buf, size_t bufLen);
+    int readListEntryAndSeekInData();
+    bool comesBefore(const SortOrder *so, const Problem *p1, const Problem *p2);
 
     const char CONST_LIST_SEPARATOR[2] = ":";
 };
